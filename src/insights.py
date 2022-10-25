48a571fb9d8459beea919cd725965dc7d7e52a6c
Rafaqfg <rafa.qfg@gmail.com>
2022-10-25 05:28:57 -0300

    feat: :sparkles: implemented get_unique_industries function

--- src/insights.py
+++ src/insights.py
@@ -54,7 +54,13 @@ def get_unique_industries(path):
     list
         List of unique industries
     """
-    return []
+    jobs = read(path)
+
+    industry = set()
+    for job in jobs:
+        if job["industry"]:
+            industry.add(job["industry"])
+    return industry
 
 
 def filter_by_industry(jobs, industry):
