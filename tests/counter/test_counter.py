662c146d8b1ee16e311609fd17dae75c86813e10
Rafaqfg <rafa.qfg@gmail.com>
2022-10-28 04:52:39 -0300

    fix:

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -2,5 +2,5 @@ from src.counter import count_ocurrences
 
 
 def test_counter():
-    assert count_ocurrences("src/jobs.py", "Python") == 1639
-    assert count_ocurrences("src/jobs.py", "JavaScript") == 122
+    assert count_ocurrences("src/jobs.csv", "Python") == 1639
+    assert count_ocurrences("src/jobs.csv", "JavaScript") == 122
