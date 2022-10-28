3899037179b40dad0696c881f9944c69c90de60a
Rafaqfg <rafa.qfg@gmail.com>
2022-10-28 06:28:54 -0300

    test: :white_check_mark: implemented tests to sorting.py file

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+
+jobs_mock = [
+    {"min_salary": 1000, "max_salary": 10000, "date_posted": "2022-11-01"},
+    {"min_salary": 1200, "max_salary": 12000, "date_posted": "2022-10-11"},
+    {"min_salary": 5000, "max_salary": 120000, "date_posted": "2022-10-13"},
+    {"min_salary": 500, "max_salary": 8123, "date_posted": "2022-08-01"},
+    {"min_salary": 654, "max_salary": 9571, "date_posted": "2022-11-01"},
+]
+
+keys = ["min_salary", "max_salary", "date_posted"]
 
 
 def test_sort_by_criteria():
-    pass
+    for criteria in keys:
+        sort_by(jobs_mock, criteria)
+        if criteria == "max_salary":
+            assert jobs_mock[0][criteria] >= jobs_mock[1][criteria]
+        else:
+            assert jobs_mock[0][criteria] <= jobs_mock[1][criteria]
